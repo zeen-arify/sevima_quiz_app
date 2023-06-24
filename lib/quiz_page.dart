bacb69e963071e5ed367bb924a429eb80f405a3d
zeen-arify <zeenarify@gmail.com>
2023-06-24 18:00:47 +0700

    Save

--- lib/quiz_page.dart
+++ lib/quiz_page.dart
@@ -17,24 +17,23 @@ class _QuizPageState extends State<QuizPage> {
   String question = '';
   String answer = '';
   String correction = '';
+  bool isQuestionGenerated = false;
 
   List<String> subjects = ['Biologi', 'Informatika', 'Sejarah'];
   List<String> difficulties = ['Mudah', 'Sedang', 'Sulit'];
 
   void generateQuestion() async {
-    final requestBody = jsonEncode(
-      {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-          {"role": "system", "content": "You are a helpful assistant."},
-          {
-            "role": "user",
-            "content":
-                "Buatkan satu pertanyaan tentang $selectedSubject dengan tingkat kesulitan yang $selectedDifficulty."
-          }
-        ]
-      },
-    );
+    final requestBody = jsonEncode({
+      "model": "gpt-3.5-turbo",
+      "messages": [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+          "role": "user",
+          "content":
+              "Buatkan satu pertanyaan tentang $selectedSubject dengan tingkat kesulitan yang $selectedDifficulty."
+        }
+      ]
+    });
 
     final response = await http.post(
       Uri.parse(apiEndpoint),
@@ -49,6 +48,9 @@ class _QuizPageState extends State<QuizPage> {
       final data = jsonDecode(response.body);
       setState(() {
         question = data['choices'][0]['message']['content'].toString();
+        isQuestionGenerated = true;
+        answer = '';
+        correction = '';
       });
     } else {
       setState(() {
@@ -88,62 +90,84 @@ class _QuizPageState extends State<QuizPage> {
     }
   }
 
+  void resetQuestion() {
+    setState(() {
+      question = '';
+      answer = '';
+      correction = '';
+      isQuestionGenerated = false;
+      selectedSubject = '';
+      selectedDifficulty = '';
+    });
+  }
+
+  void generateNewQuestion() {
+    setState(() {
+      question = '';
+      answer = '';
+      correction = '';
+      isQuestionGenerated = false;
+    });
+    generateQuestion();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Sevima Quiz App'),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Pilih Materi:',
-                  style: TextStyle(fontSize: 18.0),
-                ),
-                SizedBox(height: 8.0),
-                Wrap(
-                  spacing: 8.0,
-                  children: subjects.map((subject) {
-                    return ChoiceChip(
-                      label: Text(subject),
-                      selected: selectedSubject == subject,
-                      onSelected: (isSelected) {
-                        setState(() {
-                          selectedSubject = isSelected ? subject : '';
-                        });
-                      },
-                    );
-                  }).toList(),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
-                Text(
-                  'Pilih Kategori Pertanyaan:',
-                  style: TextStyle(fontSize: 18.0),
-                ),
-                SizedBox(height: 8.0),
-                Wrap(
-                  spacing: 8.0,
-                  children: difficulties.map((difficulty) {
-                    return ChoiceChip(
-                      label: Text(difficulty),
-                      selected: selectedDifficulty == difficulty,
-                      onSelected: (isSelected) {
-                        setState(() {
-                          selectedDifficulty = isSelected ? difficulty : '';
-                        });
-                      },
-                    );
-                  }).toList(),
-                ),
-                SizedBox(
-                  height: 16.0,
-                ),
+      appBar: AppBar(
+        title: Text('Sevima Quiz App'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Pilih Materi:',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              SizedBox(height: 8.0),
+              Wrap(
+                spacing: 8.0,
+                children: subjects.map((subject) {
+                  return ChoiceChip(
+                    label: Text(subject),
+                    selected: selectedSubject == subject,
+                    onSelected: (isSelected) {
+                      setState(() {
+                        selectedSubject = isSelected ? subject : '';
+                      });
+                    },
+                  );
+                }).toList(),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              Text(
+                'Pilih Kategori Pertanyaan:',
+                style: TextStyle(fontSize: 18.0),
+              ),
+              SizedBox(height: 8.0),
+              Wrap(
+                spacing: 8.0,
+                children: difficulties.map((difficulty) {
+                  return ChoiceChip(
+                    label: Text(difficulty),
+                    selected: selectedDifficulty == difficulty,
+                    onSelected: (isSelected) {
+                      setState(() {
+                        selectedDifficulty = isSelected ? difficulty : '';
+                      });
+                    },
+                  );
+                }).toList(),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              if (!isQuestionGenerated)
                 ElevatedButton(
                   onPressed: (selectedSubject.isNotEmpty &&
                           selectedDifficulty.isNotEmpty)
@@ -151,9 +175,7 @@ class _QuizPageState extends State<QuizPage> {
                       : null,
                   child: Text('Buat Pertanyaan'),
                 ),
-                SizedBox(
-                  height: 16.0,
-                ),
+              if (question.isNotEmpty) ...[
                 Text(
                   'Jawablah Pertanyaan Berikut:',
                   style: TextStyle(fontSize: 18.0),
@@ -180,11 +202,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
                 ElevatedButton(
                   onPressed: (answer.isNotEmpty) ? submitAnswer : null,
-                  child: Text('Kirim Jawaban'),
+                  child: Text('Cek Jawaban'),
                 ),
-                SizedBox(
-                  height: 16.0,
-                ),
+              ],
+              if (correction.isNotEmpty) ...[
                 Text(
                   'Hasil Koreksi Jawaban:',
                   style: TextStyle(fontSize: 18.0),
@@ -193,9 +214,27 @@ class _QuizPageState extends State<QuizPage> {
                   height: 8.0,
                 ),
                 Text(correction),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: resetQuestion,
+                      child: Text('Ganti Materi'),
+                    ),
+                    ElevatedButton(
+                      onPressed: generateNewQuestion,
+                      child: Text('Pertanyaan Lain'),
+                    ),
+                  ],
+                ),
               ],
-            ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
